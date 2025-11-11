1ff525ae4e2c0c796c4f5982f1c13da1f3f0ffbd
Zakloni <edjovilellaca@ittepic.edu.mx>
2025-11-11 14:30:57 -0700

    Update product.py

--- app/models/product.py
+++ app/models/product.py
@@ -2,20 +2,22 @@ import time
 from pydantic import BaseModel, Field
 from typing import Optional
 
+
 class Product(BaseModel):
     id: Optional[str] = Field(alias="_id")
-    SKU: str
+    sku: str = Field(..., alias="sku")
     name: str
     category: Optional[str] = None
     description: Optional[str] = None
-    PriceCents: Optional[int] = None
-    Currency: str
-    Stock: int
-    Images: Optional[str] = None
+    price_cents: Optional[int] = Field(None, alias="price_cents")
+    currency: str
+    stock: int
+
+    images: Optional[str] = None
     is_active: bool = True
-    Is_deleted: bool = False
-    created_At: float = Field(default_factory=time.time)
-    updated_At: float = Field(default_factory=time.time)
+    is_deleted: bool = False
+    created_at: Optional[float] = Field(default_factory=time.time, alias="created_at")
+    updated_at: Optional[float] = Field(default_factory=time.time, alias="updated_at")
 
     class Config:
         populate_by_name = True
